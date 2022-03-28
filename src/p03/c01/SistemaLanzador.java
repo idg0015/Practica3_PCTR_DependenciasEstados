6a444f551ce7a7786532514eb6cba82998352769
Nacho <Nacho@DESKTOP-MSAS6M0>
2022-03-28 10:45:24 +0200

    Arreglada la creación del objeto parque

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,7 +3,8 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(); // TODO
+		int personas_maximas = 50;
+		IParque parque = new Parque(personas_maximas);
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
